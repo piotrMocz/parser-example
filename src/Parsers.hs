e20364f313d00d60d3254c83e7f2425b20e193fc
piotrMocz <piotr.moczurad@gmail.com>
2018-11-03 22:31:51 +0100

    Next parts of the tutorial

--- src/Parsers.hs
+++ src/Parsers.hs
@@ -1,10 +1,15 @@
 module Parsers where
 
-import           Text.Megaparsec      (Parsec)
+import           Text.Megaparsec      (Parsec, )
+import qualified Text.Megaparsec      as P
 import qualified Text.Megaparsec.Char as P
 
 
-type Parser = Parsec String String
+type Parser      = Parsec String String
+type ParserError = P.ParseErrorBundle String String
+
+parse :: Parser a -> String -> Either ParserError a
+parse parser input = P.parse parser "" input
 
 openingParen :: Parser Char
 openingParen = P.char '('
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,21 @@
+import Test.Hspec
+import Parsers
 
+isLeft :: Either a b -> Bool
+isLeft (Left  _) = True
+isLeft (Right _) = False
+
+openingParenSpec :: Spec
+openingParenSpec = describe "The open paren parser" $ do
+    it "should parse the \"(\" string" $
+        parse openingParen "(" `shouldBe` Right '('
+
+    it "should parse the \"(hello)\" string" $
+        parse openingParen "(hello)" `shouldBe` Right '('
+
+    it "should fail on the \"hello\" string" $
+        isLeft (parse openingParen "hello") `shouldBe` True
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    openingParenSpec
